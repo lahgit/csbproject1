01e2e964b3acd64122563b709bf384af457b5350
lahgit <jesse.lahti@helsinki.fi>
2026-01-17 16:05:49 +0200

    link in index page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
